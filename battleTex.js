2a6ad8a84b05bc60539fd9e21fa7fe1a80a9bf79
Chris Muster <christophermuster@yahoo.co.uk>
2017-09-07 11:21:49 +0100

    Added current game files

---
+++ battleTex.js
@@ -0,0 +1,61 @@
+//The VIEW
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "border hit");
+	},
+
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "border miss");
+	}
+};
+
+/*
+//test code
+view.displayMessage("Is this working?");
+
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
+*/
+
+//The MODEL
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+	ships: [
+		{
+			locations: ["06", "16", "26"],
+		 	hits: ["", "", ""]
+		},
+		{
+			locations: ["24", "34", "44"],
+		 	hits: ["", "", ""]
+		},
+		{
+			locations: ["10", "11", "12"],
+		 	hits: ["", "", ""]
+		}
+	]
+
+	fire: function(guess) {
+		
+	}
+};
+
+
+
+
+//The CONTROLLER
